a051e1961792124a4907933d391a60545a21b5d7
FarazAyaz <ayazf313@gmail.com>
2025-03-06 18:51:21 +0500

    loader

--- src/sections/Body.jsx
+++ src/sections/Body.jsx
@@ -1,37 +1,46 @@
 import React, { useEffect, useState } from "react";
 import ProductCard from "../components/Body/ProductCard";
+import { LuLoaderCircle } from "react-icons/lu";
+import axios from "axios";
 
 const Body = () => {
   const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   const fetchData = async () => {
-    const res = await fetch("https://fakestoreapi.com/products");
-    const data = await res.json();
-    setProducts(data);
+    setLoading(true);
+    const res = await axios.get("https://fakestoreapi.com/products");
+    setLoading(false);
+    setProducts(res.data);
   };
 
   useEffect(() => {
     fetchData();
   }, []);
 
-  console.log("products", products);
   return (
     <div className="mt-[-150px] z-20 relative">
       {/* display all the products */}
-      <div className="grid grid-cols-4 gap-4">
-        {products.map((productDetails, index) => {
-          console.log("The component details for index", productDetails, index);
-          return (
-            <ProductCard
-              key={index}
-              title={productDetails.title}
-              description={productDetails.description}
-              img={productDetails.image}
-              price={productDetails.price}
-            />
-          );
-        })}
-      </div>
+      {loading ? (
+        <div className="fixed inset-0 bg-black/50  inset-0 flex items-center justify-center">
+          <LuLoaderCircle className="w-12 h-12 text-white animate-spin" />
+        </div>
+      ) : (
+        <div className="grid grid-cols-4 gap-4">
+          {products.map((productDetails, index) => {
+            console.log(productDetails)
+            return (
+              <ProductCard
+                key={index}
+                title={productDetails.title}
+                description={productDetails.description}
+                img={productDetails.image}
+                price={productDetails.price}
+              />
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 };
